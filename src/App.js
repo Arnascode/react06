10fc4ba4da2503659644f2e91755a1e376353f18
Arnascode <vilkoncius7@gmail.com>
2022-06-06 10:26:31 +0300

    hardcoded our services css somethin like that

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>HEllo</h1>
-      </header>
+    <div className='serviceSection'>
+      <section className='Title'>
+        <h1>OUR SERVICES</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </section>
+      <section className='serviceCards'>
+        <div className='serviceCard'>
+          <div className='icon'>
+            <i className='fa fa-facebook-official' aria-hidden='true'></i>
+          </div>
+          <div className='text'>
+            <h3>Air Freight</h3>
+            <p className='servP'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum harum fuga
+              ut incidunt adipisci magni!
+            </p>
+            <a href='###'>Learn more</a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
